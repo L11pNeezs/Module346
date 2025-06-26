19c6d41634a4f255ce274a2117cf57473fd85074
Guilherme Da Rocha Ferreira <guilherme.darochaferreira@liip.ch>
2025-06-26 13:44:34 +0200

    Resolve "Chore - Column Migration User"

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('surname');
             $table->string('email');
+            $table->bool('is_logged');
             $table->timestamps();
         });
     }
