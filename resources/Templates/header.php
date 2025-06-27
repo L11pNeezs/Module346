05f11ba0bf3f3cc3e4721157e5f9fc5da3847d50
Bayane Chakir <bayane.chakir@liip.ch>
2025-06-27 11:25:45 +0200

    layout - header and footer

--- resources/Templates/header.php
+++ resources/Templates/header.php
@@ -12,54 +12,40 @@ if (isset($_SESSION['id'])) {
 
 ?>
 
-<header>
-    <div class="title-container">
-        <img class="logo" src="../img/Pasted%20image.png" alt="logo">
-        <h1><a class="title" href="404.php">TTE</a></h1>
-    </div>
-    <nav class="nav-bar">
-        <ul class="nav-bar-link">
-            <li><a href="map.html">Map</a></li>
-            <li><a href="places.html">Places</a></li>
-            <li><a href="contribute">Contribute</a></li>
-        </ul>
-        <div class="auth-buttons">
-            <?php if ($username): ?>
-            <span>Welcome, <?= $username ?></span>
-            <?php else: ?>
-            <label class="modal-btn" for="modal-toggle">Login</label>
-            <?php endif; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Header</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/login.css">
+</head>
+<body>
+
+    <header>
+        <script src="/assets/js/hamburgerMenu.js"></script>
+        <div class="title-container">
+            <img class="logo" src="/assets/images/koa-la-logo.png" alt="logo">
+            <h1><a class="title" href="#">Koa-La</a></h1>
         </div>
-        <div class="rt-container">
-            <div class="col-rt-12">
-                <div>
-                    <input id="modal-toggle" type="checkbox">
-                    <label class="modal-backdrop" for="modal-toggle"></label>
-                    <div class="modal-content">
-                        <label class="modal-close-btn" for="modal-toggle"></label>
-                        <div class="tabs">
-                            <input class="radio" id="tab-1" name="tabs-name" type="radio" checked>
-                            <label for="tab-1" class="table"><span>Login</span></label>
-                            <div class="tabs-content">
-                                <form action="/logintest" method="POST">
-                                    <input type="text" name="username" placeholder="Username" required>
-                                    <input type="password" name="password" placeholder="Password" required>
-                                    <input type="submit" value="Log In">
-                                </form>
-                            </div>
-                            <input class="radio" id="tab-2" name="tabs-name" type="radio">
-                            <label for="tab-2" class="table"><span>Sign up</span></label>
-                            <div class="tabs-content">
-                                <form action="/signup" method="POST">
-                                    <input type="text" name="username" placeholder="Username" required>
-                                    <input type="text" name="name" placeholder="First Name" required>
-                                    <input type="text" name="surname" placeholder="Last Name" required>
-                                    <input type="email" name="email" placeholder="Email" required>
-                                    <input type="password" name="password" placeholder="Password" required>
-                                    <input type="submit" value="Sign Up">
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-    </nav>
-</header>
+        <nav class="nav-bar">
+
+            <ul class="nav-bar-link ">
+                <li>
+                        Map
+                </li>
+                <li>
+                        Places
+                </li>
+                <li>
+                        Contribute
+                </li>
+            </ul>
+
+            <button class="log-button login-button">Login</button>
+            </nav>
+    </header>
+    <main>
