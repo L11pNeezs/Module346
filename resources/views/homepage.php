9ddc05c90388a6a3306327451d59f094e44f53f5
Guilherme Da Rocha Ferreira <guilherme.darochaferreira@liip.ch>
2025-07-01 11:30:57 +0200

    Resolve "Hotfix - HP View"

--- resources/views/homepage.php
+++ resources/views/homepage.php
@@ -10,64 +10,43 @@ if (isset($_SESSION['id'])) {
     }
 }
 
-$restaurant = null;
-$image = null;
-$price_tier = null;
-$concept = null;
-$veggie_option = null;
-
 ?>
 
 <section class="title">
     Today you should eat :
     <div class="restaurant-suggestion">
-        <?= $restaurant ?>
+        <?= $restaurant->name ?>
     </div>
 </section>
 
 <section class="description">
-        <img src="<?= $image ?>" alt="food">
+        <img src="/assets/images/<?= $restaurant->image ?>" style="width: 400px; height: 350px;">
     <div class="title-description">
-        <?= $restaurant ?>
+        <?= $restaurant->description ?>
     </div>
-
-    <div>
-        <p class="description-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor
-            sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        </p>
+    <div class="learn-more-container">
+        <a class="learn-more-anchor" href="">Learn More</a>
     </div>
+</section>
 
-    <ul class="option-restaurant">
+<section class="restaurant-options">
+    <ul class="restaurant-list">
         <li>
-            <img src="<?= $image ?>" alt="food">
-            <?= $price_tier ?>
+            <?= $restaurant->price_tier ?>
+        </li>
+
+        <li
+            <?= $restaurant->concept ?>
         </li>
 
         <li>
-            <img src="<?= $image ?>" alt="food">
-            <?= $concept ?>
-        </li>
-
-        <li>
-            <img src="<?= $image ?>" alt="food">
-            <?= $veggie_option ?>
+            <?= $restaurant->veggie_options?>
         </li>
     </ul>
-
-
 </section>
+
+
     <section class="section-discover">
     <h3 class="discover-title">Do you want more?</h3>
-    <label class="discover-button">Discover more</label>
+    <a class="discover-button" href="/restaurants/">Discover more</a>
 </section>
