6368572955af331d7d718fb069c517e3f48fabed
Ricardo Gomes <ricardo.gomes@liip.ch>
2025-08-07 10:57:35 +0200

    Fix Rebased

--- resources/views/partials/contribute_partial.php
+++ resources/views/partials/contribute_partial.php
@@ -14,38 +14,27 @@ use App\Models\Restaurant;
         foreach (CONTRIBUTE_FORM_FIELDS as $field) { ?>
             <label for="<?= htmlspecialchars($field['id']) ?>"><?= htmlspecialchars($field['label']) ?></label>
 
-    <?php use App\Models\Restaurant;
-
-foreach (CONTRIBUTE_FORM_FIELDS as $field) { ?>
-        <label for="<?= htmlspecialchars($field['id']) ?>"><?= htmlspecialchars($field['label']) ?></label>
-
-        <?php if ($field['element'] === 'select') { ?>
-            <select id="<?= htmlspecialchars($field['id']) ?>"
-                    name="<?= htmlspecialchars($field['name']) ?>"
-                <?= ! empty($field['required']) ? 'required' : '' ?>
-                    class="<?php if (! empty($errors[$field['name']])) { ?>error<?php } ?>"
-                    onchange="<?php if ($field['name'] === 'concept' || $field['name'] === 'diet') { ?>
-                        toggleOtherInput('<?= htmlspecialchars($field['name']) ?>', this.value)
-                    <?php } ?>"
-            >
-                <?php
-            $options = [];
-            if ($field['name'] === 'concept') {
-                $concepts = Restaurant::getConcepts();
-                $options = array_combine($concepts, $concepts);
-            } elseif ($field['name'] === 'price_tier') {
-                $options = PRICE_TIERS;
-            } elseif ($field['name'] === 'diet') {
-                $diets = Restaurant::getDiets();
-                $options = array_combine($diets, $diets);
-            } ?>
-                <option value="" disabled <?= empty($old[$field['name']]) ? 'selected' : '' ?>>Select an option</option>
-                <?php foreach ($options as $value => $label) { ?>
-                    <option value="<?= htmlspecialchars($value) ?>"
-                        <?php if (($old[$field['name']] ?? '') == $value) {
-                            echo 'selected';
-                        } ?>>
-                        <?= htmlspecialchars($label) ?>
+            <?php if ($field['element'] === 'select') { ?>
+                <select id="<?= htmlspecialchars($field['id']) ?>"
+                        name="<?= htmlspecialchars($field['name']) ?>"
+                    <?= ! empty($field['required']) ? 'required' : '' ?>
+                        class="<?php if (! empty($errors[$field['name']])) { ?>error<?php } ?>"
+                        onchange="<?php if ($field['name'] === 'concept' || $field['name'] === 'diet') { ?>
+                            toggleOtherInput('<?= htmlspecialchars($field['name']) ?>', this.value)
+                        <?php } ?>"
+                >
+                    <?php
+                    $options = [];
+                    if ($field['name'] === 'concept') {
+                        $concepts = Restaurant::getConcepts();
+                        $options = array_combine($concepts, $concepts);
+                    } elseif ($field['name'] === 'price_tier') {
+                        $options = PRICE_TIERS;
+                    } elseif ($field['name'] === 'diet') {
+                        $diets = Restaurant::getDiets();
+                        $options = array_combine($diets, $diets);
+                    } ?>
+                    <option value="" disabled <?= empty($old[$field['name']]) ? 'selected' : '' ?>>Select an option
                     </option>
                     <?php foreach ($options as $value => $label) { ?>
                         <option value="<?= htmlspecialchars($value) ?>"
@@ -95,50 +84,30 @@ foreach (CONTRIBUTE_FORM_FIELDS as $field) { ?>
                 </div>
             <?php } else { ?>
                 <input
-                    type="text"
-                    id="<?= htmlspecialchars($field['name']) ?>_other"
-                    name="<?= htmlspecialchars($field['name']) ?>_other"
-                    placeholder="Please specify"
-                    style="display:<?= (($old[$field['name']] ?? '') == '__other__') ? 'block' : 'none' ?>; margin-top:5px;"
-                    value="<?= htmlspecialchars($old[$field['name'].'_other'] ?? '') ?>"
+                    type="<?= htmlspecialchars($field['type']) ?>"
+                    id="<?= htmlspecialchars($field['id']) ?>"
+                    name="<?= htmlspecialchars($field['name']) ?>"
+                    <?= ! empty($field['required']) ? 'required' : '' ?>
+                    value="<?= htmlspecialchars($old[$field['name']] ?? '') ?>"
+                    <?= ! empty($field['placeholder']) ? 'placeholder="'.htmlspecialchars($field['placeholder']).'"' : '' ?>
+                    class="<?php if (! empty($errors[$field['name']])) { ?>error<?php } ?>"
                 >
             <?php } ?>
-        <?php } elseif ($field['element'] === 'textarea') { ?>
-            <textarea id="<?= htmlspecialchars($field['id']) ?>"
-                      name="<?= htmlspecialchars($field['name']) ?>" <?= ! empty($field['required']) ? 'required' : '' ?>
-                      class="<?php if (! empty($errors[$field['name']])) { ?>error<?php } ?>"
-            ><?= htmlspecialchars($old[$field['name']] ?? '') ?></textarea>
-        <?php } elseif ($field['element'] === 'checkbox') { ?>
-            <input
-                type="checkbox"
-                id="<?= htmlspecialchars($field['id']) ?>"
-                name="<?= htmlspecialchars($field['name']) ?>"
-                <?= ! empty($field['required']) ? 'required' : '' ?>
-                value="1"
-                <?php if (! empty($old[$field['name']]) && $old[$field['name']] == '1') {
-                    echo 'checked';
-                } ?>
-                class="<?php if (! empty($errors[$field['name']])) { ?>error<?php } ?>"
-        <?php } else { ?>
-            <input
-                type="<?= htmlspecialchars($field['type']) ?>"
-                id="<?= htmlspecialchars($field['id']) ?>"
-                name="<?= htmlspecialchars($field['name']) ?>"
-                <?= ! empty($field['required']) ? 'required' : '' ?>
-                value="<?= htmlspecialchars($old[$field['name']] ?? '') ?>"
-                <?= ! empty($field['placeholder']) ? 'placeholder="'.htmlspecialchars($field['placeholder']).'"' : '' ?>
-                class="<?php if (! empty($errors[$field['name']])) { ?>error<?php } ?>"
-            >
+
+            <?php if (! empty($errors[$field['name']])) { ?>
+                <span class="error"><?= htmlspecialchars($errors[$field['name']]) ?></span>
+            <?php } ?>
+
         <?php } ?>
-
-        <?php if (! empty($errors[$field['name']])) { ?>
-            <span class="error"><?= htmlspecialchars($errors[$field['name']]) ?></span>
-        <?php } ?>
-
-    <?php } ?>
-
-    <div>
-        <button class="btn" type="submit">Submit</button>
+        <p><strong>* Required Fields</strong></p>
+        <div>
+            <button class="btn" type="submit">Submit</button>
+        </div>
+    </form>
+    <div id="response-modal" style="display:none; position:fixed; top:0; left:0; width:100vw; height:100vh; background:rgba(0,0,0,0.5); z-index:9999; align-items:center; justify-content:center;">
+        <div style="background:#fff; padding:2em; border-radius:8px; max-width:400px; margin:auto; position:relative;">
+            <span id="modal-message"></span>
+        </div>
     </div>
     <script>
         function toggleOtherInput(field, value) {
